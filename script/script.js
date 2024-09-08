828840a34c32d2623f617e561df4900f169f725d
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-08 21:04:47 +0300

    Solution pushed(mobile)

--- script/script.js
+++ script/script.js
@@ -5,14 +5,15 @@ plusButton.forEach((elem) => {
     let title = elem.parentElement;
     let accordion = title.parentElement;
     let content = accordion.querySelector('.content > .text');
-    console.log(elem.src.toString());
-    if (elem.src.toString()=="http://127.0.0.1:5500/assets/images/icon-plus.svg") {
+    let t = elem.src.toString().split('/');
+    t = t[t.length - 1];
+    if (t=="icon-plus.svg") {
       content.style.height = "60px";
       elem.src = "./assets/images/icon-minus.svg";
     }
     else {
       content.style.height = "0px";
-      elem.src = "http://127.0.0.1:5500/assets/images/icon-plus.svg";
+      elem.src = "./assets/images/icon-plus.svg";
     }
   });
 });
